870ffe19c05faaf8ab3a6fe0b1354f3d71ff4c3f
Denis Voronin <mystimind@mail.ru>
2019-06-20 19:40:01 +0300

    linter and aliases configurate

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        cwd: 'babelrc',
+        root: ['.'],
+        extensions: ['.js', '.ios.js', '.android.js'],
+        alias: {
+          'todo-list': './',
+          '@': './src',
+        },
+      },
+    ],
+  ],
 };
--- index.js
+++ index.js
@@ -2,8 +2,8 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import { AppRegistry } from 'react-native';
+import App from '@/App'; // eslint-disable-line
+import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
